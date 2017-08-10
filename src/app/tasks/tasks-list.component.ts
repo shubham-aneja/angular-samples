fb06b57c54bc8703ebfaa292fe00d7a29602c74c
Shubham Aneja <shubham.aneja@3pillar.corp>
2017-08-10 10:23:45 +0530

    link visit css and global css added

--- src/app/tasks/tasks-list.component.ts
+++ src/app/tasks/tasks-list.component.ts
@@ -4,6 +4,10 @@ import {ITask} from './task'
 import {ActivatedRoute} from '@angular/router'
 import uuid from 'uuid'
 
+
+
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'tasks-list',
   moduleId: module.id,
@@ -27,6 +31,26 @@ export class TasksListComponent implements OnInit {
     this.sub = this._route.params.subscribe(params => {
       this.status = params.status
     });
+
+    this._route.paramMap
+      .switchMap((params) => {
+        console.log(' params are here again, this time with the help of switchMap ', params)
+        /*this console wont get printed untill subscibe is used post to this function call*/
+        return ['abc']
+      })
+      .subscribe(status => {
+
+        console.log('param is', status)
+        //return status
+      });
+      //COmment this out, but remember, subscribe only works with stream respose,
+      //  like a [array] or a string(array of character) lets say 'abc' or ['a','b','c']
+
+
+    /*  this.route.paramMap
+     .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+     .subscribe(hero => this.hero = hero);*/
+
   }
 
   ngOnDestroy() {
@@ -37,26 +61,26 @@ export class TasksListComponent implements OnInit {
     value.trim()
     if (!value)return;
     this.taskList = [...this.taskList,
-      {id:uuid.v4(), title: value, isCompleted: this.status === 'completed', priority: 1}
+      {id: uuid.v4(), title: value, isCompleted: this.status === 'completed', priority: 1}
     ]
     this.taskTitle = '';
   }
 
-  toggleCompletionStatus(task:ITask, index: number = 2):void {
-    this.taskList = this.taskList.map( (currentTask, currentIndex) => {
-      if(task.id=== currentTask.id){
+  toggleCompletionStatus(task:ITask, index:number = 2):void {
+    this.taskList = this.taskList.map((currentTask, currentIndex) => {
+      if (task.id === currentTask.id) {
         return {
           ...currentTask,
           isCompleted: !currentTask.isCompleted
         }
-      }else {
+      } else {
         return currentTask
       }
     })
 
   }
 
-  onTaskPriorityPress(priority:number): void {
+  onTaskPriorityPress(priority:number):void {
     console.log(`Task clicked with priority ${priority} in parent/task-list component`)
   }
 }
