74f3dac4dae5654ab0902a077a7056c10ce877ec
Shubham Aneja <shubham.aneja@3pillar.corp>
2017-08-28 13:35:56 +0530

    animations transition issue resolved

--- src/app/samples/angular-animations/angular-animations.component.ts
+++ src/app/samples/angular-animations/angular-animations.component.ts
@@ -10,11 +10,9 @@ import {
 
 const Heroes = [
   {id: 1, name: 'shubham', state: 'active'},
-  {id: 2, name: 'Tpg', state: 'inactive'}
-]
-const IndianHeroes = [
-  {id: 1, name: 'Akshay', state: 'in'},
-  {id: 2, name: 'Ranbir', state: 'out'}
+  {id: 2, name: 'Tpg', state: 'inactive'},
+  {id: 3, name: 'Noida', state: 'active'},
+  {id: 4, name: 'UP', state: 'inactive'}
 ]
 
 
@@ -27,86 +25,66 @@ const IndianHeroes = [
     trigger('heroState', [
       state('inactive', style({
         backgroundColor: '#eee',
-        transform: 'scale(1)'
+        transform: 'scale(0.8)'
       })),
       state('active', style({
         backgroundColor: '#cfd8dc',
         transform: 'scale(1.1)'
       })),
-      transition('active <=> inactive, * <=> inactive, * <=> active', animate(3000)),
-      //transition('* <=> active', animate('1000ms ease-in')),
-      //transition('active <=> inactive', animate('500ms ease-in'))
+      transition('inactive=> active', animate(1000,
+         keyframes([
+          style({transform: 'translateX(-100%)'}),
+          style({transform: 'translateX(100%)'}),
+          style({transform: 'translateX(-20%)'}),
+          style({transform: 'translateX(20%)'}),
+          style({transform: 'translateX(0)'}),
+          style({transform: 'translateX(0)'}),
+        ])
+      )),
+      transition('active => inactive', animate(1000,
+         keyframes([
+          style({transform: 'translateX(-100%)'}),
+          style({transform: 'translateX(100%)'}),
+          style({transform: 'translateX(-20%)'}),
+          style({transform: 'translateX(20%)'}),
+          style({transform: 'translateX(0)'}),
+        ])
+      )),
     ]),
-    trigger('indianHeroState', [
-      state('in', style({
-        backgroundColor: '#eee',
-        transform: 'scale(1)'
-      })),
-      state('out', style({
-        backgroundColor: '#cfd8dc',
-        transform: 'scale(1.1)'
-      })),
-      transition(':enter', [
-        style({transform: 'translateX(-100%)'}),
-        animate('1000ms  1000ms ease-in')
-      ]),
-      //transition('void => in', [
-      //  style({transform: 'translateX(-100%)'}),
-      //  animate('1000ms ease-in')
-      //]),
 
-      //transition('out=> void, in => void', [
-      //  style({transform: 'translateX(200%)'}),
-      //  animate('1000ms ease-in')
-      //]),
-      transition(':leave', [
-        style({transform: 'translateX(200%)'}),
-        animate('1000ms 1000ms ease-in')
-      ]),
-
-      transition('in <=> out', animate('500ms ease-in'))
-    ])
   ]
 })
 
 export class AngularAnimationsComponent {
   title:string = 'Angular animations';
-  heroState:string = 'active';
   heroes:any[] = Heroes;
   tempHero:any = Heroes[0];
-  indianHeroes:any[] = IndianHeroes;
 
-  toggleState():void {
-    this.heroState = getUpdatedState(this.heroState);
-  }
 
   toggleStateOfHero(id):void {
-    this.heroes = this.heroes.map((hero) => {
+    /*updating in reference do not altering transitions
+    I guess this is because the object is getting created again and again due to
+    change in reference...
+    This c
+    .. */
+
+/*    this.heroes = this.heroes.map((hero) => {
+     if (hero.id === id) {
+     return {...hero, state: getUpdatedState(hero.state)}
+     } else {
+     return hero
+     }
+     });*/
+    this.heroes.forEach((hero) => {
       if (hero.id === id) {
-        return {...hero, state: getUpdatedState(hero.state)}
-      } else {
-        return hero
+        hero.state = getUpdatedState(hero.state)
       }
     })
   }
 
-  toggleIndianHero(id):void {
-    this.indianHeroes = this.indianHeroes.map((hero) => {
-      if (hero.id === id) {
-        return {...hero, state: getIndianHeroUpdatedState(hero.state)}
-      } else {
-        return hero
-      }
-    })
-  }
-
-  deleteIndianHero(id:number):void {
-    this.indianHeroes = this.indianHeroes.filter((indianHero) =>indianHero.id !== id)
-  }
-
-  insertIndianHero() {
-    this.indianHeroes = [...this.indianHeroes,
-      {id: this.indianHeroes.length, name: new Date(), state: 'in'}
+  insertHero() {
+    this.heroes = [...this.heroes,
+      {id: this.heroes.length, name: new Date(), state: 'active'}
     ]
   }
 }
@@ -115,9 +93,3 @@ export class AngularAnimationsComponent {
 const getUpdatedState = (prevState) => {
   return prevState = prevState == 'active' ? 'inactive' : 'active'
 }
-
-
-const getIndianHeroUpdatedState = (prevState) => {
-  return prevState = prevState == 'in' ? 'out' : 'in'
-}
-
