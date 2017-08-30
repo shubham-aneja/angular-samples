8134accc16ee876d06e694d2aa65ced43a36c2da
Shubham Aneja <shubham.aneja@3pillar.corp>
2017-08-30 18:39:10 +0530

    component interactions in a seprate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,9 @@ import { AppComponent } from './app.component';
 import {TemplateSyntaxComponent} from './samples/template-syntax/template-syntax.component'
 import {AngularAnimationsComponent} from './samples/angular-animations/angular-animations.component'
 import {InternationalizationComponent} from './samples/internationalization/internationalization.component'
-import {ComponentInteractionsComponent} from './samples/component-interactions/component-interactions.component'
-import {CompInterInputAlias} from './samples/component-interactions/comp-inter-input-alias.component'
-import {CompInterIntercept} from './samples/component-interactions/comp-inter-input-intercept-onchanges.component'
-import {CompInterLocalVar} from './samples/component-interactions/comp-inter-local-var.component'
-import {CompInterMissionControl} from './samples/component-interactions/interactionsViaService/comp-inter-mission-control'
-import {CompInterAstronaut} from './samples/component-interactions/interactionsViaService/comp-inter-astronaut'
 
 
+import {ComponentInteractionsModule} from './samples/component-interactions/component-interactions.module'
 
 @NgModule({
   declarations: [
@@ -24,27 +19,24 @@ import {CompInterAstronaut} from './samples/component-interactions/interactionsV
     TemplateSyntaxComponent,
     AngularAnimationsComponent,
     InternationalizationComponent,
-    ComponentInteractionsComponent,
-    CompInterInputAlias,
-    CompInterIntercept,
-    CompInterLocalVar,
-    CompInterAstronaut,
-    CompInterMissionControl
   ],
   imports: [
+    ComponentInteractionsModule,
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {path: 'template-syntax', component: TemplateSyntaxComponent},
       {path: 'angular-animations', component: AngularAnimationsComponent},
       {path: 'internationalization', component: InternationalizationComponent},
-      {path: 'component-interactions', component: ComponentInteractionsComponent},
+
+
       {path: '**', redirectTo: "template-syntax"},
     ]),
     HttpModule,
     BrowserAnimationsModule
   ],
-  //providers:[TaskService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
---
+++ src/app/samples/component-interactions/component-interactions.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {NgModule} from '@angular/core'
+
+import {ComponentInteractionsComponent} from './component-interactions.component'
+import {CompInterInputAlias} from './comp-inter-input-alias.component'
+import {CompInterIntercept} from './comp-inter-input-intercept-onchanges.component'
+import {CompInterLocalVar} from './comp-inter-local-var.component'
+import {CompInterMissionControl} from './interactionsViaService/comp-inter-mission-control'
+import {CompInterAstronaut} from './interactionsViaService/comp-inter-astronaut'
+
+@NgModule({
+  declarations:[
+    ComponentInteractionsComponent,
+    CompInterInputAlias,
+    CompInterIntercept,
+    CompInterLocalVar,
+    CompInterMissionControl,
+    CompInterAstronaut
+  ],
+  imports:[
+    BrowserModule,
+    RouterModule.forChild([
+      {path: 'component-interactions', component: ComponentInteractionsComponent},
+    ])
+  ]
+})
+
+export class ComponentInteractionsModule {
+
+}
